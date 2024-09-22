ca6c88938d02f17ff3728a6736198c608a616263
TioZiio <davidmaf@gmail.com>
2024-09-22 09:36:51 -0300

    Configurando antigos e novos Recursos
    Corrigindo a variavel first_name do banco de dados, antes 'fist_name' depois 'first_name';
    Adicionando o recurso de inserir imagem para os contatos;

--- README.md
+++ README.md
@@ -2,8 +2,17 @@
 Este projeto de Django será construido junto ao Curso Udemy Python.
 O objetivo final das aulas é a criação de uma Agenda.
 
+Informações importantes:
+1. Pasta migrations do App - Registra e aplica configurações do models (BD);
+
+Comandos importantes:
+1. runserver - Inicia o servidor;
+2. createsuperuser - Cria o Super Usuário;
+3. makemigrations - Atualiza as modificações nos models;
+4. migrate - Aplica novas Atualizações;
+
 Ferramentas:
-1. Adicionar/Editar/Apagar Contatos
-2. Criar perfil de Usuário.
-3. Validação de usuário e senha.
-4. Validação de senha forte.
+1. Adicionar/Editar/Apagar Contatos;
+2. Criar perfil de Usuário;
+3. Validação de usuário e senha;
+4. Validação de senha forte;
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+
+    def __str__(self) -> str:
+        return (f'{self.first_name} {self.last_name}')
+
+    def save(self, *args, **kwargs):
+        fields_str = ['first_name','last_name']
+
+        for field in fields_str:
+            value = getattr(self, field, None)
+            if isinstance(value, str):
+                setattr(self, field, value.title())
+
+        super(Contact, self).save(*args, **kwargs)
+
